26061aad5ac989dcfaaf60698a1a8a86cd04a446
Adam Vintr <Vintr.Adam@stud.vassboskovice.cz>
2024-06-04 09:18:37 +0200

    update

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -1,7 +1,19 @@
 Imports System
 
 Module Program
-  Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+    Sub Main(args As String())
+        Dim cislo, cislo2, vysledek As Integer
+        Do
+            Console.Write("Napiš èíslo: ")
+            cislo = Console.ReadLine()
+            Console.Write("Napiš 2.èíslo: ")
+            cislo2 = Console.ReadLine
+            If cislo / 2 = 5 Then
+            End If
+
+            vysledek = cislo + cislo2
+
+            Console.WriteLine($"výsledek tìchto èísel je {vysledek} ")
+        Loop
+    End Sub
 End Module
